c3440a82c0727fb9369df3d18d909639abb37703
Sergei Khadanovich <khadanovich.sergey@gmail.com>
2024-01-08 18:10:24 +0300

    Labeled statements are not supported.

--- result4j-processor/src/test/java/dev/khbd/result4j/javac/option/ReturnTest.java
+++ result4j-processor/src/test/java/dev/khbd/result4j/javac/option/ReturnTest.java
@@ -6,6 +6,7 @@ import dev.khbd.result4j.core.Option;
 import dev.khbd.result4j.javac.AbstractPluginTest;
 import org.testng.annotations.Test;
 
+import javax.tools.Diagnostic;
 import java.lang.reflect.Method;
 
 /**
@@ -56,5 +57,35 @@ public class ReturnTest extends AbstractPluginTest {
         greet = (Option<String>) method.invoke(null, 10);
         assertThat(greet.isEmpty()).isFalse();
         assertThat(greet.get()).isEqualTo("Sergei");
+    }
+
+    @Test
+    public void propagate_unwrapCallInLabeledReturn_propagate() {
+        String source = """
+                package cases.in_return;
+                                
+                import dev.khbd.result4j.core.Option;
+                                
+                public class Main {
+                                
+                    public static Option<String> greet(int index) {
+                        label:
+                        return Option.some(name(index).unwrap());
+                    }
+                                
+                    private static Option<String> name(int index) {
+                        if (index == 0) {
+                            return Option.none();
+                        }
+                        return Option.some(index < 0 ? "Alex" : "Sergei");
+                    }
+                }
+                """;
+
+        CompilationResult result = compiler.compile(new PluginOptions(true), "cases/in_return/Main.java", source);
+
+        assertThat(result.isFail()).isTrue();
+        assertThat(result.getErrors()).extracting(Diagnostic::toString)
+                .anyMatch(msg -> msg.contains(" Unsupported position for unwrap method call"));
     }
 }

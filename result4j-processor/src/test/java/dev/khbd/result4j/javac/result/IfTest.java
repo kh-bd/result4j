c33767d272f20fe2a64771a60427ee7da5b6dd9f
Sergei Khadanovich <khadanovich.sergey@gmail.com>
2024-06-13 00:00:53 +0300

    Support unwrap call in if conditions. #116

--- result4j-processor/src/test/java/dev/khbd/result4j/javac/result/IfTest.java
+++ result4j-processor/src/test/java/dev/khbd/result4j/javac/result/IfTest.java
@@ -15,67 +15,175 @@ import java.lang.reflect.Method;
 public class IfTest extends AbstractPluginTest {
 
     @Test
-    public void propagate_inCondition_failCompilation() {
+    public void propagate_inCondition() throws Exception {
         String source = """
                 package cases.if_statement;
-                                
-                import java.util.Random;
+                
                 import dev.khbd.result4j.core.Result;
-                                
+                
                 public class Main {
-                                
-                    public static Result<String, String> getName() {
-                        if (random().unwrap().booleanValue()) {
+                
+                    public static Result<String, String> getName(int flag) {
+                        if (getFlag(flag).unwrap().booleanValue()) {
                             return Result.success("Alex");
-                        } else {
+                        }
+                        return Result.success("Sergei");
+                    }
+                
+                    public static Result<String, Boolean> getFlag(int flag) {
+                        if (flag == 0) {
                             return Result.error("error");
                         }
-                    }
-                    
-                    public static Result<String, Boolean> random() {
-                        var rnd = new Random();
-                        if (rnd.nextBoolean()) {
-                            return Result.success(rnd.nextBoolean());
-                        }
-                        return Result.error("error");
+                        return Result.success(flag > 0);
                     }
                 }
                 """;
 
         CompilationResult result = compiler.compile(new PluginOptions(true), "cases/if_statement/Main.java", source);
 
-        assertThat(result.isFail()).isTrue();
-        assertThat(result.getErrors()).extracting(Diagnostic::toString)
-                .anyMatch(msg -> msg.contains(" Unsupported position for unwrap method call"));
+        assertThat(result.isFail()).isFalse();
+
+        ClassLoader classLoader = result.classLoader();
+        Class<?> clazz = classLoader.loadClass("cases.if_statement.Main");
+        Method method = clazz.getMethod("getName", int.class);
+
+        // call with 0
+        Result<String, String> name = (Result<String, String>) method.invoke(null, 0);
+        assertThat(name.isError()).isTrue();
+        assertThat(name.getError()).isEqualTo("error");
+
+        // call with positive number
+        name = (Result<String, String>) method.invoke(null, 10);
+        assertThat(name.isError()).isFalse();
+        assertThat(name.get()).isEqualTo("Alex");
+
+        // call with negative number
+        name = (Result<String, String>) method.invoke(null, -10);
+        assertThat(name.isError()).isFalse();
+        assertThat(name.get()).isEqualTo("Sergei");
     }
 
-
     @Test
-    public void propagate_inElseIfCondition_failCompilation() {
+    public void propagate_inComplexConditionButAtLeftSide() throws Exception {
         String source = """
                 package cases.if_statement;
-                                
-                import java.util.Random;
+                
                 import dev.khbd.result4j.core.Result;
-                                
+                
                 public class Main {
-                                
-                    public static Result<String, String> getName() {
-                        if (false) {
+                
+                    public static Result<String, String> getName(int flag) {
+                        if (getFlag(flag).unwrap().booleanValue() && flag > 0) {
                             return Result.success("Alex");
-                        } else if (random().unwrap().booleanValue()) {
-                            return Result.success("Sergei");
-                        } else {
+                        }
+                        return Result.success("Sergei");
+                    }
+                
+                    public static Result<String, Boolean> getFlag(int flag) {
+                        if (flag == 0) {
                             return Result.error("error");
                         }
+                        return Result.success(flag > 0);
                     }
-                    
-                    public static Result<String, Boolean> random() {
-                        var rnd = new Random();
-                        if (rnd.nextBoolean()) {
-                            return Result.success(rnd.nextBoolean());
+                }
+                """;
+
+        CompilationResult result = compiler.compile(new PluginOptions(true), "cases/if_statement/Main.java", source);
+
+        assertThat(result.isFail()).isFalse();
+
+        ClassLoader classLoader = result.classLoader();
+        Class<?> clazz = classLoader.loadClass("cases.if_statement.Main");
+        Method method = clazz.getMethod("getName", int.class);
+
+        // call with 0
+        Result<String, String> name = (Result<String, String>) method.invoke(null, 0);
+        assertThat(name.isError()).isTrue();
+        assertThat(name.getError()).isEqualTo("error");
+
+        // call with positive number
+        name = (Result<String, String>) method.invoke(null, 10);
+        assertThat(name.isError()).isFalse();
+        assertThat(name.get()).isEqualTo("Alex");
+
+        // call with negative number
+        name = (Result<String, String>) method.invoke(null, -10);
+        assertThat(name.isError()).isFalse();
+        assertThat(name.get()).isEqualTo("Sergei");
+    }
+
+    @Test
+    public void propagate_inElseIfCondition_failCompilation() throws Exception {
+        String source = """
+                package cases.if_statement;
+                
+                import dev.khbd.result4j.core.Result;
+                
+                public class Main {
+                
+                    public static Result<String, String> getName(int flag) {
+                        if (flag == 0) {
+                            return Result.error("error");
+                        } else if (getFlag(flag).unwrap().booleanValue()) {
+                            return Result.success("Alex");
+                        }
+                        return Result.success("Sergei");
+                    }
+                
+                    public static Result<String, Boolean> getFlag(int flag) {
+                        if (flag == 0) {
+                            return Result.error("error");
+                        }
+                        return Result.success(flag > 0);
+                    }
+                }
+                """;
+
+        CompilationResult result = compiler.compile(new PluginOptions(true), "cases/if_statement/Main.java", source);
+
+        assertThat(result.isFail()).isFalse();
+
+        ClassLoader classLoader = result.classLoader();
+        Class<?> clazz = classLoader.loadClass("cases.if_statement.Main");
+        Method method = clazz.getMethod("getName", int.class);
+
+        // call with 0
+        Result<String, String> name = (Result<String, String>) method.invoke(null, 0);
+        assertThat(name.isError()).isTrue();
+        assertThat(name.getError()).isEqualTo("error");
+
+        // call with positive number
+        name = (Result<String, String>) method.invoke(null, 10);
+        assertThat(name.isError()).isFalse();
+        assertThat(name.get()).isEqualTo("Alex");
+
+        // call with negative number
+        name = (Result<String, String>) method.invoke(null, -10);
+        assertThat(name.isError()).isFalse();
+        assertThat(name.get()).isEqualTo("Sergei");
+    }
+
+    @Test
+    public void propagate_inRightSideOfComplexCondition_failCompilation() {
+        String source = """
+                package cases.if_statement;
+                
+                import dev.khbd.result4j.core.Result;
+                
+                public class Main {
+                
+                    public static Result<String, String> getName(int flag) {
+                        if (flag > 0 && getFlag(flag).unwrap().booleanValue()) {
+                            return Result.success("Alex");
                         }
                         return Result.error("error");
+                    }
+                
+                    public static Result<String, Boolean> getFlag(int flag) {
+                        if (flag == 0) {
+                            return Result.error("error");
+                        }
+                        return Result.success(flag > 0);
                     }
                 }
                 """;
@@ -91,11 +199,11 @@ public class IfTest extends AbstractPluginTest {
     public void propagate_inThenBlock_success() throws Exception {
         String source = """
                 package cases.if_statement;
-                                
+                
                 import dev.khbd.result4j.core.Result;
-                                
+                
                 public class Main {
-                                
+                
                     public static Result<String, String> getName(boolean flag) {
                         if (flag) {
                             return Result.success(getName().unwrap().toUpperCase());
@@ -103,7 +211,7 @@ public class IfTest extends AbstractPluginTest {
                             return Result.error("error");
                         }
                     }
-                                
+                
                     public static Result<String, String> getName() {
                         return Result.success("Alex");
                     }
@@ -133,11 +241,11 @@ public class IfTest extends AbstractPluginTest {
     public void propagate_inElseBlock_success() throws Exception {
         String source = """
                 package cases.if_statement;
-                                
+                
                 import dev.khbd.result4j.core.Result;
-                                
+                
                 public class Main {
-                                
+                
                     public static Result<String, String> getName(boolean flag) {
                         if (flag) {
                             return Result.error("error");
@@ -145,7 +253,7 @@ public class IfTest extends AbstractPluginTest {
                             return Result.success(getName().unwrap().toUpperCase());
                         }
                     }
-                                
+                
                     public static Result<String, String> getName() {
                         return Result.success("Alex");
                     }
@@ -175,16 +283,16 @@ public class IfTest extends AbstractPluginTest {
     public void propagate_inThenStatement_success() throws Exception {
         String source = """
                 package cases.if_statement;
-                                
+                
                 import dev.khbd.result4j.core.Result;
-                                
+                
                 public class Main {
-                                
+                
                     public static Result<String, String> getName(boolean flag) {
                         if (flag) return Result.success(getName().unwrap().toUpperCase());
                         else return Result.error("error");
                     }
-                                
+                
                     public static Result<String, String> getName() {
                         return Result.success("Alex");
                     }
@@ -214,16 +322,16 @@ public class IfTest extends AbstractPluginTest {
     public void propagate_inElseStatement_success() throws Exception {
         String source = """
                 package cases.if_statement;
-                                
+                
                 import dev.khbd.result4j.core.Result;
-                                
+                
                 public class Main {
-                                
+                
                     public static Result<String, String> getName(boolean flag) {
                         if (flag) return Result.error("error");
                         else return Result.success(getName().unwrap().toUpperCase());
                     }
-                                
+                
                     public static Result<String, String> getName() {
                         return Result.success("Alex");
                     }

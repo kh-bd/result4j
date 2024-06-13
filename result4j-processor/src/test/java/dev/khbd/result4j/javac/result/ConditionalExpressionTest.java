9d89b4caf834e8bf9105a5c44e5a1560c168e898
Sergey Khadanovich <khadanovich.sergey@gmail.com>
2024-06-13 19:42:25 +0300

    Merge pull request #126 from kh-bd/117-in-ternary-operator
    Support unwrap call in ternary operator conditions. #117

--- result4j-processor/src/test/java/dev/khbd/result4j/javac/result/ConditionalExpressionTest.java
+++ result4j-processor/src/test/java/dev/khbd/result4j/javac/result/ConditionalExpressionTest.java
@@ -2,10 +2,12 @@ package dev.khbd.result4j.javac.result;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import dev.khbd.result4j.core.Result;
 import dev.khbd.result4j.javac.AbstractPluginTest;
 import org.testng.annotations.Test;
 
 import javax.tools.Diagnostic;
+import java.lang.reflect.Method;
 
 /**
  * @author Sergei_Khadanovich
@@ -13,26 +15,31 @@ import javax.tools.Diagnostic;
 public class ConditionalExpressionTest extends AbstractPluginTest {
 
     @Test
-    public void propagate_unwrapCallInCondition_failCompilation() {
-        String source = "package cases.conditional_expression;\n" +
-                        "\n" +
-                        "import java.util.Random;\n" +
-                        "import dev.khbd.result4j.core.Result;\n" +
-                        "\n" +
-                        "public class Main {\n" +
-                        "\n" +
-                        "    public static Result<String, String> getName() {\n" +
-                        "        return random().unwrap() ? Result.success(\"Alex\") : Result.success(\"Sergei\");\n" +
-                        "    }\n" +
-                        "\n" +
-                        "    public static Result<String, Boolean> random() {\n" +
-                        "        var rnd = new Random();\n" +
-                        "        if (rnd.nextBoolean()) {\n" +
-                        "            return Result.success(rnd.nextBoolean());\n" +
-                        "        }\n" +
-                        "        return Result.error(\"error\");\n" +
-                        "    }\n" +
-                        "}\n";
+    public void propagate_nestedThen_failCompilation() {
+        String source = """
+                package cases.conditional_expression;
+                
+                import java.util.Random;
+                import dev.khbd.result4j.core.Result;
+                
+                public class Main {
+                
+                    static Random rnd = new Random();
+                
+                    public static Result<String, String> getName() {
+                        return rnd.nextBoolean()
+                          ? (random().unwrap() ? Result.success("Alex") : Result.error("error"))
+                          : Result.success("Alex");
+                    }
+                
+                    public static Result<String, Boolean> random() {
+                        if (rnd.nextBoolean()) {
+                            return Result.success(rnd.nextBoolean());
+                        }
+                        return Result.error("error");
+                    }
+                }
+                """;
 
         CompilationResult result = compiler.compile(new PluginOptions(true), "cases/conditional_expression/Main.java", source);
 
@@ -42,27 +49,153 @@ public class ConditionalExpressionTest extends AbstractPluginTest {
     }
 
     @Test
+    public void propagate_nestedElse_failCompilation() {
+        String source = """
+                package cases.conditional_expression;
+                
+                import java.util.Random;
+                import dev.khbd.result4j.core.Result;
+                
+                public class Main {
+                
+                    static Random rnd = new Random();
+                
+                    public static Result<String, String> getName() {
+                        return rnd.nextBoolean()
+                          ? Result.success("Alex")
+                          : (random().unwrap() ? Result.success("Alex") : Result.error("error"));
+                    }
+                
+                    public static Result<String, Boolean> random() {
+                        if (rnd.nextBoolean()) {
+                            return Result.success(rnd.nextBoolean());
+                        }
+                        return Result.error("error");
+                    }
+                }
+                """;
+
+        CompilationResult result = compiler.compile(new PluginOptions(true), "cases/conditional_expression/Main.java", source);
+
+        assertThat(result.isFail()).isTrue();
+        assertThat(result.getErrors()).extracting(Diagnostic::toString)
+                .anyMatch(msg -> msg.contains(" Unsupported position for unwrap method call"));
+    }
+
+    @Test
+    public void propagate_nestedCondition() throws Exception {
+        String source = """
+                package cases.conditional_expression;
+                
+                import dev.khbd.result4j.core.Result;
+                
+                public class Main {
+                
+                    public static Result<String, String> getName(int flag) {
+                        return (flag(flag).unwrap() ? true : false)
+                          ? Result.success("Alex")
+                          : Result.success("Sergei");
+                    }
+                
+                    public static Result<String, Boolean> flag(int flag) {
+                        if (flag == 0) {
+                            return Result.error("error");
+                        }
+                        return Result.success(flag > 0);
+                    }
+                }
+                """;
+
+        CompilationResult result = compiler.compile(new PluginOptions(true), "cases/conditional_expression/Main.java", source);
+
+        assertThat(result.isSuccess()).isTrue();
+
+        ClassLoader classLoader = result.classLoader();
+        Class<?> clazz = classLoader.loadClass("cases.conditional_expression.Main");
+        Method method = clazz.getMethod("getName", int.class);
+
+        Result<String, String> unwrapped = (Result<String, String>) method.invoke(null, 0);
+        assertThat(unwrapped.isError()).isTrue();
+        assertThat(unwrapped.getError()).isEqualTo("error");
+
+        unwrapped = (Result<String, String>) method.invoke(null, 10);
+        assertThat(unwrapped.isSuccess()).isTrue();
+        assertThat(unwrapped.get()).isEqualTo("Alex");
+
+        unwrapped = (Result<String, String>) method.invoke(null, -10);
+        assertThat(unwrapped.isSuccess()).isTrue();
+        assertThat(unwrapped.get()).isEqualTo("Sergei");
+    }
+
+    @Test
+    public void propagate_unwrapCallInCondition() throws Exception {
+        String source = """
+                package cases.conditional_expression;
+                
+                import dev.khbd.result4j.core.Result;
+                
+                public class Main {
+                
+                    public static Result<String, String> getName(int flag) {
+                        return flag(flag).unwrap()
+                          ? Result.success("Alex")
+                          : Result.success("Sergei");
+                    }
+                
+                    public static Result<String, Boolean> flag(int flag) {
+                        if (flag == 0) {
+                            return Result.error("error");
+                        }
+                        return Result.success(flag > 0);
+                    }
+                }
+                """;
+
+        CompilationResult result = compiler.compile(new PluginOptions(true), "cases/conditional_expression/Main.java", source);
+
+        assertThat(result.isSuccess()).isTrue();
+
+        ClassLoader classLoader = result.classLoader();
+        Class<?> clazz = classLoader.loadClass("cases.conditional_expression.Main");
+        Method method = clazz.getMethod("getName", int.class);
+
+        Result<String, String> unwrapped = (Result<String, String>) method.invoke(null, 0);
+        assertThat(unwrapped.isError()).isTrue();
+        assertThat(unwrapped.getError()).isEqualTo("error");
+
+        unwrapped = (Result<String, String>) method.invoke(null, 10);
+        assertThat(unwrapped.isSuccess()).isTrue();
+        assertThat(unwrapped.get()).isEqualTo("Alex");
+
+        unwrapped = (Result<String, String>) method.invoke(null, -10);
+        assertThat(unwrapped.isSuccess()).isTrue();
+        assertThat(unwrapped.get()).isEqualTo("Sergei");
+    }
+
+    @Test
     public void propagate_unwrapCallInThen_failCompilation() {
-        String source = "package cases.conditional_expression;\n" +
-                        "\n" +
-                        "import java.util.Random;\n" +
-                        "import dev.khbd.result4j.core.Result;\n" +
-                        "\n" +
-                        "public class Main {\n" +
-                        "\n" +
-                        "    public static Result<String, String> getName() {\n" +
-                        "        return condition() ? Result.success(nameInternal().unwrap().toUpperCase()) : Result.error(\"error\");\n" +
-                        "    }\n" +
-                        "\n" +
-                        "    public static boolean condition() {\n" +
-                        "        var rnd = new Random();\n" +
-                        "        return rnd.nextBoolean();\n" +
-                        "    }\n" +
-                        "\n" +
-                        "    public static Result<String, String> nameInternal() {\n" +
-                        "        return Result.success(\"Alex\");\n" +
-                        "    }\n" +
-                        "}\n";
+        String source = """
+                package cases.conditional_expression;
+                
+                import java.util.Random;
+                import dev.khbd.result4j.core.Result;
+                
+                public class Main {
+                
+                    public static Result<String, String> getName() {
+                        return condition() ? Result.success(nameInternal().unwrap().toUpperCase()) : Result.error("error");
+                    }
+                
+                    public static boolean condition() {
+                        var rnd = new Random();
+                        return rnd.nextBoolean();
+                    }
+                
+                    public static Result<String, String> nameInternal() {
+                        return Result.success("Alex");
+                    }
+                }
+                """;
 
         CompilationResult result = compiler.compile(new PluginOptions(true), "cases/conditional_expression/Main.java", source);
 
@@ -73,26 +206,28 @@ public class ConditionalExpressionTest extends AbstractPluginTest {
 
     @Test
     public void propagate_unwrapCallInElse_failCompilation() {
-        String source = "package cases.conditional_expression;\n" +
-                        "\n" +
-                        "import java.util.Random;\n" +
-                        "import dev.khbd.result4j.core.Result;\n" +
-                        "\n" +
-                        "public class Main {\n" +
-                        "\n" +
-                        "    public static Result<String, String> getName() {\n" +
-                        "        return condition() ?  Option.none() : Result.success(nameInternal().unwrap().toUpperCase());\n" +
-                        "    }\n" +
-                        "\n" +
-                        "    public static boolean condition() {\n" +
-                        "        var rnd = new Random();\n" +
-                        "        return rnd.nextBoolean();\n" +
-                        "    }\n" +
-                        "\n" +
-                        "    public static Result<String, String> nameInternal() {\n" +
-                        "        return Result.success(\"Alex\");\n" +
-                        "    }\n" +
-                        "}\n";
+        String source = """
+                package cases.conditional_expression;
+                
+                import java.util.Random;
+                import dev.khbd.result4j.core.Result;
+                
+                public class Main {
+                
+                    public static Result<String, String> getName() {
+                        return condition() ?  Option.none() : Result.success(nameInternal().unwrap().toUpperCase());
+                    }
+                
+                    public static boolean condition() {
+                        var rnd = new Random();
+                        return rnd.nextBoolean();
+                    }
+                
+                    public static Result<String, String> nameInternal() {
+                        return Result.success("Alex");
+                    }
+                }
+                """;
 
         CompilationResult result = compiler.compile(new PluginOptions(true), "cases/conditional_expression/Main.java", source);
 
